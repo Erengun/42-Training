ee53b7b3520785155306ccc71ae504ca58a1212f
Eren Gun <70534390+Erengun@users.noreply.github.com>
2021-12-07 11:19:29 +0300

    Piscine Reloaded go brrr

---
+++ PiscineRE/ex18/test.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+    write(1,&c,1);
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    i = 1;
+    while(i < argc)
+    {
+        while(*argv[i])
+        {
+            ft_putchar(*argv[i]);
+            argv[i]++;
+        }
+        ft_putchar('\n');
+        i++;
+    }
+    return (0);
+}
---
+++ PiscineRE/ex19/ft_sort_params.c
@@ -0,0 +1,12 @@
+void    ft_putchar(char c);
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    i = 1;
+    while(i < argc)
+    {
+        
+    }
+}

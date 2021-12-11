76aea5f75d2dcab70203a51f967c6bfbb64adbca
Eren Gun <70534390+Erengun@users.noreply.github.com>
2021-12-11 15:21:26 +0300

    try

--- Libft/Batu.c
+++ Libft/Batu.c
@@ -1,25 +1,34 @@
 int   isBothOddOrEven(int a, int b)
 {
-      if((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && a % 2 != 0))
-            return (1);
-      return (0);
+	if((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && a % 2 != 0))
+		return (1);
+	return (0);
 }
 
 
 char* getLargestNumber (char *num) 
 {
-      //5324798520
-      5 Oddmax  = 3  9     
-      3 Evenmax = 0  7   
-      2              8    
-      4              4    
-      7              5   
-      9              5    
-      8              2 
-      5              3   
-      2              2 
-      0              0  
-      int Oddmax;
-      int Evenmax = 0;
-      int i = 0;
+	/*5324798520
+	5 Oddmax  = 9  9     
+	3 Evenmax = 8  7   
+	2              8    
+	4              4    
+	7              5   
+	9              5    
+	8              2 
+	5              3   
+	2              2 
+	0              0  */
+	int Oddmax;
+	int Evenmax = 0;
+	int i = 0;
+
+	while(num[i])
+	{
+		if(Oddmax < num[i] && num[i] % 2 != 0)
+			Oddmax = num[i];
+		if(Oddmax < num[i] && num[i] % 2 == 0)
+			Evenmax = num[i];
+		i++;
+	}
 }

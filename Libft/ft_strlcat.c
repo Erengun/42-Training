0057526c40faadeb13353d1c1871a0021fcf1adc
Eren Gun <70534390+Erengun@users.noreply.github.com>
2021-12-17 22:56:58 +0300

    Header + pdf

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+
+int   ft_strlen(char *str)
+{
+      int i;
+
+      i = 0;
+      while(str[i])
+            i++;
+      return (i);
+}
+
+size_t strlcat(char *dst, const char *src, size_t size)
+{
+    
+
+int   main()
+{
+      printf("%s",strlcat("Hello","Bro",2);
+      return 1;
+}
---
+++ Libft/libft.h
@@ -0,0 +1,8 @@
+#include "libft.h"
+#ifndef libft
+#define libft
+
+int   ft_isalnum(int a);
+int   ft_strlen(char *str);
+
+#endif
